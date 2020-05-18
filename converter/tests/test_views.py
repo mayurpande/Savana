08201530baa24e1bf9ba470946fe6f972791a194
mayur <mayurpande.uk@gmail.com>
2020-05-18 16:51:55 +0200

    created mock tests to handle redirect and save file

--- converter/tests/test_views.py
+++ converter/tests/test_views.py
@@ -1,4 +1,10 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+from django.urls import reverse
+from unittest.mock import patch, call
+
+from functional_tests.test_pdf_to_text_converter_page import PDF_FILE
+from converter.forms import PdfConverterForm
 
 
 # Create your tests here.
@@ -15,11 +21,32 @@ class HomePageTest(RouteTemplateTester):
 
     def test_home_page_returns_template(self):
 
-        self.route('/converter/', 'home.html')
+        self.route(reverse('index'), 'home.html')
 
 
 class PdfConverterTest(RouteTemplateTester):
 
     def test_pdf_converter_page_returns_template(self):
 
-        self.route('/converter/pdf/', 'pdf.html')
+        self.route(reverse('pdf'), 'pdf.html')
+
+    @patch('converter.views.handle_upload_file')
+    def test_POST_form_redirects(self, mock_handle_upload_file):
+        with open(PDF_FILE, 'rb') as f:
+            upload_file = f.read()
+            post_dict = {'title': 'test title'}
+            form_data = {'file': SimpleUploadedFile(f.name, upload_file)}
+            response = self.client.post(reverse('pdf'), form_data)
+            self.assertEqual(mock_handle_upload_file.called, True)
+            self.assertRedirects(response, reverse('converted_pdf'))
+
+    @patch('converter.views.handle_upload_file')
+    def test_POST_form_saves_file_as_pdf(self, mock_handle_upload_file):
+        with open(PDF_FILE, 'rb') as f:
+            upload_file = f.read()
+            post_dict = {'title': 'test title'}
+            form_data = {'file': SimpleUploadedFile(f.name, upload_file)}
+            self.client.post(reverse('pdf'), form_data)
+            self.assertEqual(mock_handle_upload_file.called, True)
+
+
