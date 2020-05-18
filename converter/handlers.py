2977d175acc3b7f749f2beaccc4d8c13bb363db0
mayur <mayurpande.uk@gmail.com>
2020-05-18 16:53:32 +0200

    created handle_upload_file function

---
+++ converter/handlers.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+
+
+def handle_upload_file(attachment):
+
+    with open(settings.MEDIA_ROOT + attachment.name, 'wb') as destination:
+        for chunk in attachment.chunks():
+            destination.write(chunk)
+
