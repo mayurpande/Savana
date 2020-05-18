8cfbaf97f35b60207ecd1fa22d1ee55a86ef7eaf
mayur <mayurpande.uk@gmail.com>
2020-05-18 16:53:06 +0200

    wrote post method for pdf view

--- converter/views.py
+++ converter/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib import messages
+
+from converter.forms import PdfConverterForm
+from converter.handlers import handle_upload_file
 
 
 # Create your views here.
-from converter.forms import PdfConverterForm
-
-
 def index(request):
-
     return render(request, 'home.html')
+
+
+def converted_pdf(request):
+    return HttpResponse('ok')
 
 
 def pdf(request):
     form = PdfConverterForm()
 
+    if request.method == 'POST':
+        form = PdfConverterForm(request.POST, request.FILES)
+        handle_upload_file(request.FILES['file'])
+        return redirect('converted_pdf')
+
     return render(request, 'pdf.html', {'form': form})
+
+
+
