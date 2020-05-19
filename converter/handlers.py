cd6add4209c9177e6c78b118efd2a6e08b1b4494
mayur <mayurpande.uk@gmail.com>
2020-05-19 11:29:22 +0200

    added docstring to functions

--- converter/handlers.py
+++ converter/handlers.py
@@ -1,10 +1,27 @@
 from django.conf import settings
+import pdftotext
 
 
 def handle_upload_file(attachment):
 
+    """Saves request.FILES attachment to MEDIA_ROOT"""
+
     with open(settings.MEDIA_ROOT + attachment.name, 'wb') as destination:
         for chunk in attachment.chunks():
             destination.write(chunk)
+
+
+def handle_converting_pdf_to_text(attachment):
+
+    """Opens pdf and converts to text using pdftotext library."""
+
+    with open(settings.MEDIA_ROOT + attachment.name, "rb") as f:
+        pdf = pdftotext.PDF(f)
+        content_text = ''
+        for page in pdf:
+            content_text += page.strip() + "\n"
+        with open(settings.MEDIA_ROOT + "converted_text.txt", 'w') as p:
+            p.write(content_text)
+
 
 
