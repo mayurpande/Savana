56e3dee47f77b3dacd00ec9bde68cf226a17ef94
mayur <mayurpande.uk@gmail.com>
2020-05-19 12:45:11 +0200

    removed redundant url for redirect

--- converter/urls.py
+++ converter/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('pdf/', views.pdf, name='pdf'),
-    path('converted_pdf/', views.converted_pdf, name='converted_pdf')
 ]
