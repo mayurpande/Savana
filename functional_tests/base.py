2ffc9f950d88502794b0d99db47e598edfefd6be
mayur <mayurpande.uk@gmail.com>
2020-05-20 17:28:06 +0200

    added save to disk to firefox profile

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,11 +1,19 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from pathlib import Path
+import os
 
 
 class Base(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.fp = webdriver.FirefoxProfile()
+        self.fp.set_preference("browser.download.folderList", 2)
+        path = Path(os.environ['HOME'])
+        self.fp.set_preference("browser.download.manager.showWhenStarting", False)
+        self.fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/json")
+        self.fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain")
+        self.browser = webdriver.Firefox(firefox_profile=self.fp)
 
     def tearDown(self):
         self.browser.quit()
