a6c27aa8175e24a83a479e528e90089d7cdeca21
mayur <mayurpande.uk@gmail.com>
2020-05-20 15:02:38 +0200

    asserted equals test

--- converter/tests/test_views.py
+++ converter/tests/test_views.py
@@ -129,9 +129,16 @@ class TxtConverterTest(RouteTemplateTester):
         self.assertEqual(new_patient_data.mr_num, 240804)
 
     def test_response_returns_json_dump_file(self):
+
+        """Assert equals file has been downloaded"""
+
         file_path = os.path.join(settings.MEDIA_ROOT, "data.json")
         with open(file_path, 'r') as f:
             response = HttpResponse(f.read(), content_type='text/json')
             response['Content-Disposition'] = 'attachment; filename=' + '"' + os.path.basename(file_path)
-            return response
+            self.assertEquals(
+                response.get('Content-Disposition'),
+                'attachment; filename=' + '"' + os.path.basename(file_path)
+            )
+
 
